a9d41c134586974b167403ed04bea8431d3e4147
Ramona Snider <41329897+rlsnider@users.noreply.github.com>
2022-07-19 00:40:07 -0700

    BreadCRUD Part 6

--- views/show.jsx
+++ views/show.jsx
@@ -6,34 +6,34 @@ function Show ({bread, index}) {
   // console.log(bread.name)
     return (
       <Default>
-  <h3>{bread.name}</h3>
-  <p>
-    and it
-    {
-      bread.hasGluten
-      ? <span> does </span>
-      : <span> does NOT </span>
-    }
-    have gluten.
-  </p>
-  <img src={bread.image} alt={bread.name} />
-  <li><a href="/breads">Go home</a></li>
-  
+      <h3>{bread.name}</h3>
+      <p>
+        and it
+        {
+          bread.hasGluten
+          ? <span> does </span>
+          : <span> does NOT </span>
+        }
+        have gluten.
+      </p>
+      <img src={bread.image} alt={bread.name} />
+    
 
-  <form action={`/breads/${index}?_method=DELETE`} method="POST">
+      <li><a href="/breads">Go home</a></li>
+
+      <a href={`/breads/${index}/edit`}><button>Edit</button></a>
+
+      
+<form action={`/breads/${index}?_method=DELETE`} method="POST">
   <input type='submit' value="DELETE"/>
 </form>
 
- 
-
-
-
-</Default>
-
-     
+    </Default>
     )
 }
 
-    
+
+
 module.exports = Show
+
 
