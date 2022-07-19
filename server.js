ae722a7d1b93055373bfcabb06b4f81365a87200
Ramona Snider <41329897+rlsnider@users.noreply.github.com>
2022-07-18 23:41:49 -0700

    BreadCRUD part 5

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //DEPENDENCIES
 const express = require('express');
 const breads = require('./controllers/breads_controller.js');
+const methodOverride = require('method-override')
 
 require('dotenv').config();
 const PORT = process.env.PORT
@@ -13,6 +14,7 @@ app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
 
 
 //ROUTES
