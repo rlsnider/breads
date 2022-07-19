9ef642a79900a833001ad1343398272fafb24ca9
Ramona Snider <41329897+rlsnider@users.noreply.github.com>
2022-07-18 20:31:07 -0700

    restart, did lesson3 be-3

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -1,66 +1,15 @@
+//required
 const express = require('express')
 const breads = express.Router()
 const Bread = require('../models/bread.js')
 
-// Route
-breads.get('/', (req, res) => {
-  Bread.find().then((foundBreads) => {
-    res.render('index', {
-      breads: foundBreads,
-      title: 'Index Page',
-    });
-  });
-});
-
-
-// INDEX
-breads.get('/:id', (req, res) => {
-  Bread.findById(req.params.id)
-      .then(foundBread => {
-          res.render('show', {
-              bread: foundBread
-          })
-      })
+//INDEX
+breads.get('/', (req, res)=>{
+  res.send(Bread)
 })
-
-
-
-// CREATE
-breads.post('/', (req, res) => {
-  if(!req.body.image) {
-      req.body.image = undefined 
-  }
-  if(req.body.hasGluten === 'on') {
-    req.body.hasGluten = true
-  } else {
-    req.body.hasGluten = false
-  }
-  Bread.create(req.body)
-  res.redirect('/breads')
+//SHOW 
+breads.get('/:arrayIndex', (req, res)=> {
+  res.send(Bread[req.params.arrayIndex])
 })
-
-
-
-//New
-breads.get('/new', (req, res) => {
-  res.render('new')
-})
-//delete
-breads.delete('/:indexArray', (req, res)=>{
-  Bread.splice(req.params.indexArray, 1)
-  res.status(303).redirect('/breads')
-})
-// SHOW
-breads.get('/:arrayIndex', (req, res) => {
-  if (Bread[req.params.arrayIndex]) {
-    res.render('Show', {
-      bread:Bread[req.params.arrayIndex]
-    })
-  } else {
-    res.render('404')
-  }
-})
-
-  
 
 module.exports = breads
--- server.js
+++ server.js
@@ -1,42 +1,21 @@
-// DEPENDENCIES
+//DEPENDENCIES
 const express = require('express')
-const mongoose = require('mongoose')
 
-
-
-
- 
-// CONFIGURATION
-require('dotenv').config()
+require('dotenv').config();
 const PORT = process.env.PORT
 const app = express()
+console.log(PORT)
 
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
-  () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
-)
-
-// MIDDLEWARE
-app.set('views', __dirname + '/views')
-app.set('view engine', 'jsx')
-app.engine('jsx', require('express-react-views').createEngine())
-app.use(express.static('public'))
-app.use(express.urlencoded({extended: true}))
-
-// ROUTES
-app.get('/', (req, res) => {
+//ROUTES
+//Landing Page
+app.get('/', (req, res)=>{
   res.send('Welcome to an Awesome App about Breads!')
 })
-//Breads route
-const breadsController = require('./controllers/breads_controller.js')
+//Breads- Index page
+const breadsController = require ('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
 
-// 404 Page
-app.get( '*', (req, res) => {
-  console.log('I am trying to read the error 404 page')
-  res.send('404')
+//LISTEN
+app.listen (PORT, ()=>{
+  console.log ("we are at port", PORT);
 })
-// LISTEN
-app.listen(PORT, () => {
-  console.log('nomming at port', PORT);
-})
-
