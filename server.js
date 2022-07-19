5f271a0576aa97317c3863dc0daea2bd809e760f
Ramona Snider <41329897+rlsnider@users.noreply.github.com>
2022-07-19 02:31:40 -0700

    unsuccessfully added mongoose

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 //DEPENDENCIES
 const express = require('express');
 const breads = require('./controllers/breads_controller.js');
-const methodOverride = require('method-override')
+const methodOverride = require('method-override');
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+  () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+)
+no
 
 require('dotenv').config();
 const PORT = process.env.PORT
