24f282801a8477d1b8bd615e6aedecfb8ec2c6a3
Ramona Snider <41329897+rlsnider@users.noreply.github.com>
2022-07-21 01:14:30 -0700

    doing debugs

--- server.js
+++ server.js
@@ -1,56 +1,29 @@
-//DEPENDENCIES
-const express = require('express');
-const breads = require('./controllers/breads_controller.js');
-const methodOverride = require('method-override');
-const mongoose = require('mongoose');
-mongoose.connect('process.env.MONGO_URI', {useNewUrlParser: true, useUnifiedTopology: true},
- () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
-)
+// DEPENDENCIES
+const express = require('express')
 
-
-require('dotenv').config();
+// CONFIGURATION
+require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
-console.log(PORT)
 
 // MIDDLEWARE
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
-app.use(express.static('public'))
-app.use(express.urlencoded({extended: true}))
-app.use(methodOverride('_method'))
 
 
-//ROUTES
-//Landing Page
-app.get('/', (req, res)=>{
-  res.send('Welcome to an Awesome App about Breads!')
-})
-//Breads- Index page
-const breadsController = require ('./controllers/breads_controller.js')
-app.use('/breads', breadsController)
+// ROUTES
+app.get('/', (req, res) => {
+    res.send('Welcome to an Awesome App about Breads')
+  })
+  
+  // Breads
+  const breadsController = require('./controllers/breads_controller.js')
+  app.use('/breads', breadsController)
+  
 
-// Show
-breads.get('/:arrayIndex', (req, res) => {
-  if (Bread[req.params.arrayIndex]) {
-    res.render('Show', {
-      bread:Bread[req.params.arrayIndex]
-    })
-  } else {
-    res.render('error404')
-  }
+// LISTEN
+app.listen(PORT, () => {
+  console.log('nomming at port', PORT);
 })
 
-
-//404 Page
-app.get('*', (req, res)=> {
-  res.render('error404')
-})
-
-
-
-//LISTEN
-app.listen (PORT, ()=>{
-  console.log ("we are at port", PORT);
-})
--- views/layouts/default.jsx
+++ views/layouts/default.jsx
@@ -1,34 +1,21 @@
+const React = require('react')
 
-//REQUIRED
-const React = require ('react')
-
-function Default (html){
-  return (
-    <html>
+function Default(html) {
+    return (
+      <html>
       <head>
-       <title>{html.title || 'Default'}</title>
-
+        <title>{html.title || 'Default'}</title>
       </head>
       <body>
-        <div className="wrapper">
-          <header>
-            <h1><a href="/breads">BreadCRUD</a></h1>
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossOrigin="anonymous" />
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css" integrity="sha512-EZLkOqwILORob+p0BXZc+Vm3RgJBOe1Iq/0fiI7r/wJgzOFZMlsqTa29UEl6v6U6gsV4uIpsNZoV32YZqrCRCQ==" crossOrigin="anonymous" />
-            <link rel ="stylesheet" href = "/main.css" />
-          </header>
-        <div className="container">
-          {html.children}
-        </div>
-        </div>
+        <h1>HTML Rendred!</h1>
+        {html.children}
       </body>
-
       </html>
-  )
-}
+    )
+  }
+  
 
-      // <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" />
-      
-     
+module.exports = Default
 
-      module.exports = Default
+
+
