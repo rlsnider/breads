78cea79d95513fa14f70e772324e012d483c160f
Ramona Snider <41329897+rlsnider@users.noreply.github.com>
2022-07-21 22:30:50 -0700

    edit and update routes complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -32,6 +32,14 @@ breads.get('/new', (req, res) => {
   res.status(303).render('new')
 })
 
+// EDIT
+breads.get('/:indexArray/edit', (req, res) => {
+  res.render('edit', {
+    bread: Bread[req.params.indexArray],
+    index: req.params.indexArray
+  })
+})
+
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
@@ -48,6 +56,17 @@ breads.get('/:arrayIndex', (req, res) => {
 breads.delete('/:indexArray', (req, res) => {
   Bread.splice(req.params.indexArray, 1)
   res.status(303).redirect('/breads')
+})
+
+// UPDATE
+breads.put('/:arrayIndex', (req, res) => {
+  if(req.body.hasGluten === 'on'){
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  Bread[req.params.arrayIndex] = req.body
+  res.redirect(`/breads/${req.params.arrayIndex}`)
 })
 
   
