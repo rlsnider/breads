6cf90996bca5407ff511eff24ad612473c4e511f
Ramona Snider <41329897+rlsnider@users.noreply.github.com>
2022-07-21 19:25:43 -0700

    delete route complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -23,27 +23,33 @@ breads.post('/', (req, res) => {
     req.body.hasGluten = false
   }
   Bread.push(req.body)
-  res.redirect('/breads')
+  res.status(303).redirect('/breads')
 })
 
 
 // NEW
 breads.get('/new', (req, res) => {
-  res.render('new')
+  res.status(303).render('new')
 })
 
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
     if (Bread[req.params.arrayIndex]) {
-      res.render('Show', {
-        bread:Bread[req.params.arrayIndex]
+      res.status(303).render('Show', {
+        bread:Bread[req.params.arrayIndex],
+        index: req.params.arrayIndex,
       })
     } else {
-      res.render('error404')
+      res.status(303).render('error404')
     }
   })
-  
+  // DELETE
+breads.delete('/:indexArray', (req, res) => {
+  Bread.splice(req.params.indexArray, 1)
+  res.status(303).redirect('/breads')
+})
+
   
 
 module.exports = breads
