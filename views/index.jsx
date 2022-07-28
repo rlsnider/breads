514cb9ecd358eb5d29178cf0216ccce7c1fc85c4
Ramona Snider <41329897+rlsnider@users.noreply.github.com>
2022-07-28 15:28:13 -0700

    added 'new' page

--- views/index.jsx
+++ views/index.jsx
@@ -16,6 +16,10 @@ function Index ({breads, title}) {
           })
         }
        </ul>
+       <div className="newButton">
+  <a href="/breads/new"><button>Add a new bread</button></a>
+</div>
+
       </Default>
     )
 }
--- views/new.jsx
+++ views/new.jsx
@@ -1,3 +1,36 @@
+const React = require('react')
+const Default = require('./layouts/Default')
+
+function New () {
+    return (
+      <Default>
+        <h2>Add a new bread</h2>
+        <form action="/breads" method="POST">
+            <label htmlFor="name">Name</label>
+            <input
+            type="text"
+            name="name"
+            id="name" 
+            required
+            />
+            <lavel htmlFor="image">Image</lavel>
+            <input
+            type="text"
+            name="image"
+            id="image" />
+            <label htmlFor="hasGluten">Has Gluten?</label>
+            <input type="submit" />
+
+        </form>
+        <div className="backButton">
+            <a href="/breads"><button>Go back to the index</button></a>
+        </div>
+
+      </Default>
+    )
+}
+
+module.exports = New
 
 
 
