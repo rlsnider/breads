e1b21c5584ee0bdc3f62aa3413a08cf2f55f2cd2
Ramona Snider <41329897+rlsnider@users.noreply.github.com>
2022-07-28 04:41:33 -0700

    added 404 page

--- views/error404.jsx
+++ views/error404.jsx
@@ -1,1 +1,15 @@
+const React = require('react')
+const Default = require('./layouts/Default')
 
+function error404() {
+    return(
+        <Default>
+            <div>
+                <h1 id="red-message">Oops, Page Not Found</h1>
+            </div>
+        </Default>
+    )
+    
+}
+
+module.exports = error404
