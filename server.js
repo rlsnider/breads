01944def693e0f11c3fbcbb9e49e4e5ff6cc355e
Ramona Snider <41329897+rlsnider@users.noreply.github.com>
2022-07-28 04:19:03 -0700

    did index and show data

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const app = express()
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
 
 // ROUTES
@@ -17,10 +18,15 @@ app.get('/', (req, res) => {
   res.send('Welcome to an Awesome App about Breads')
 })
 
+
 // Breads
 const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
 
+//404 page
+app.get('*', (req, res) => {
+  res.send('404')
+})
 
 // LISTEN
 app.listen(PORT, () => {
