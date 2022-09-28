247f53ed5e46d5d9f246bb849ba6c17104e3d404
Ramona Snider <41329897+rlsnider@users.noreply.github.com>
2022-09-28 14:16:56 -0700

    breadCRUD Part3

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -12,7 +12,14 @@ breads.get('/', (req, res) => {
 })
 // SHOW 
 breads.get('/:arrayIndex', (req, res) => {
-    res.send(Bread[req.params.arrayIndex])
+    if(Bread[req.params.arrayIndex]){
+        res.render('Show', {
+            bread: Bread[req.params.arrayIndex],
+            title: 'Show Page'
+    })
+} else {
+    res.send('404')
+  }
 })
 
 module.exports = breads
